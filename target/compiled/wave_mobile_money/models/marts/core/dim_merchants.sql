3ad47fac5d7e612f1979b4b1b42a349d153052fd
aoakeem <akimakeem@yahoo.com>
2026-05-21 22:48:41 -0400

    Remove build artifacts from tracking

--- target/compiled/wave_mobile_money/models/marts/core/dim_merchants.sql
+++
@@ -1,9 +0,0 @@
-select
-    merchant_id,
-    merchant_name,
-    category,
-    country,
-    status,
-    created_at,
-    updated_at
-from "wave"."main_staging"."stg_merchants"
--- target/compiled/wave_mobile_money/models/marts/core/dim_wallets.sql
+++
@@ -1,9 +0,0 @@
-select
-    wallet_id,
-    user_created_at,
-    kyc_tier,
-    country,
-    is_agent,
-    status,
-    updated_at
-from "wave"."main_staging"."stg_wallets"
